fbe34e21a16ed7234437c5357e2222ab7ea8c220
WilliamHBuhrigIV <wbuhr001@odu.edu>
2023-11-29 18:26:29 -0500

    modified PWM test to include frequency shifting

--- local_computer/gpio_pwm_test.py
+++ local_computer/gpio_pwm_test.py
@@ -1,12 +1,20 @@
 import time
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+frequency = 500
+duty = 0.5
 GPIO.setmode(GPIO.BOARD)
 p_gpio = 11
 GPIO.setup(p_gpio, GPIO.OUT)
-p = GPIO.PWM(p_gpio, 60)
-p.start(1)
-input('Press return to decrease:')
-p.ChangeDutyCycle(0.5)
-input('Press return to stop:')
+p = GPIO.PWM(p_gpio, frequency)
+p.start(0.5)
+print('Duty Cycle set at '+str(duty*100)+'%.')
+try:
+    while True:
+        frequency = input('Select a Frequency to Test:')
+        print('Currently Testing at '+str(frequency)+'hz.')
+        p.ChangeFrequency(frequency)
+except KeyboardInterrupt:
+    pass
+print('\nEnding Session at '+str(frequency)+'hz.')
 p.stop()
 GPIO.cleanup()
