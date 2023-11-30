87317a6b285432c225d903f242cdb2c59fbceb67
William Harris Buhrig IV <williamhbuhrigiv@protonmail.com>
2023-11-29 20:56:31 -0500

    added Test for Shifted Frequency

--- local_computer/gpio_pwm_test.py
+++ local_computer/gpio_pwm_test.py
@@ -1,18 +1,31 @@
 import time
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 frequency = 500
 duty = 0.5
 GPIO.setmode(GPIO.BOARD)
 p_gpio = 11
 GPIO.setup(p_gpio, GPIO.OUT)
 p = GPIO.PWM(p_gpio, frequency)
-p.start(0.5)
+p.start(duty)
 print('Duty Cycle set at '+str(duty*100)+'%.')
 try:
-    while True:
-        frequency = input('Select a Frequency to Test:')
-        print('Currently Testing at '+str(frequency)+'hz.')
-        p.ChangeFrequency(frequency)
+    print('Currently Testing at 500 hz.')
+    p.ChangeFrequency(500)
+    input('Press Return to Test Next Frequency')
+    print('Currently Testing at 583 hz.')
+    p.ChangeFrequency(583)
+    input('Press Return to Test Next Frequency')
+    print('Currently Testing at 666 hz.')
+    p.ChangeFrequency(666)
+    input('Press Return to Test Next Frequency')
+    print('Currently Testing at 783 hz.')
+    p.ChangeFrequency(783)
+    input('Press Return to Test Next Frequency')
+    print('Currently Testing at 900 hz.')
+    p.ChangeFrequency(900)
+    input('Press Return to Stop')
+    print('500 Hz Frequency Test with Shift.')
+    p.ChangeFrequency(0.7444*500+78.046)
 except KeyboardInterrupt:
     pass
 print('\nEnding Session at '+str(frequency)+'hz.')
